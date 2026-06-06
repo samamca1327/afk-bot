f7c252cff33538133354e6e4deb048253e7491f0
samamca1327 <160362924+samamca1327@users.noreply.github.com>
2026-06-06 23:37:41 +0300

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('Afk bot is running!'));
 function keep_alive() {
   app.listen(port, '0.0.0.0', () => console.log(`Afk bot web server listening on port ${port}`));
